8563fcfb581f1ee44d2d5cfe8421ee81e414bde2
micha1026 <yomi1026@naver.com>
2020-01-02 11:06:10 +0900

    Updates

---
+++ precache-manifest.a40b5dace1d2f1890923ca91b8fb1ad8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc97a887b54af4e3bc567181c6ccec2",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "ba6c53fac859cc8c1b55",
+    "url": "/movie_app/static/css/main.086f153a.chunk.css"
+  },
+  {
+    "revision": "34efeaef8a43fcf0a5f8",
+    "url": "/movie_app/static/js/2.31ce3539.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.31ce3539.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba6c53fac859cc8c1b55",
+    "url": "/movie_app/static/js/main.962c1c93.chunk.js"
+  },
+  {
+    "revision": "3dd3f4ef526798ea7005",
+    "url": "/movie_app/static/js/runtime-main.ec52c1d1.js"
+  }
+]);
